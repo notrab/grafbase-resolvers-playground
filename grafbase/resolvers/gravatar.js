f97c40a3ecee88bf5c7c90140914a855668dd997
Jamie Barton <jamie@notrab.dev>
2023-03-23 18:47:24 +0000

    Create gravatar.js

---
+++ grafbase/resolvers/gravatar.js
@@ -0,0 +1,9 @@
+import crypto from 'crypto'
+
+export default function Resolver(root, args, context, info) {
+//  const { email } = root
+
+  const emailHash = crypto.createHash('md5').update('jamie@grafbase.com').digest('hex')
+
+  return `https://www.gravatar.com/avatar/${emailHash}?d=identicon`
+}
